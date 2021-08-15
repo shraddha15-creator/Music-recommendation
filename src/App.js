d41743f48f3c8c972165f95afd53cc4f4e12e00d
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-08-15 21:47:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import "./styles.css";
+import { useState } from "react";
+import songsData from "./songs.json";
+
+export default function App() {
+  var [genre, setGenre] = useState(songsData.lofi);
+  var [artist, setArtist] = useState(false);
+
+  function lofiMusicClickHandler() {
+    setArtist(true);
+    setGenre(songsData.lofi);
+  }
+
+  function remixMusicClickHandler() {
+    setArtist(true);
+    setGenre(songsData.remix);
+  }
+
+  function evergreenMusicClickHandler() {
+    setArtist(true);
+    setGenre(songsData.evergreen);
+  }
+
+  function zumbaGymMusicClickHandler() {
+    setArtist(true);
+    setGenre(songsData.zumba);
+  }
+
+  return (
+    <div className="App">
+      <h1 id="heading">Music Recommendation</h1>
+      <p id="app-details">Checkout My Playlist. Select any Genre to listen!</p>
+
+      <div className="genres">
+        <button id="lofi" onClick={lofiMusicClickHandler}>
+          Lofi Bollywood
+        </button>
+        <button id="remix" onClick={remixMusicClickHandler}>
+          Bollywood Remix
+        </button>
+        <button id="evergreen" onClick={evergreenMusicClickHandler}>
+          EverGreen
+        </button>
+        <button id="zumba-gym" onClick={zumbaGymMusicClickHandler}>
+          Zumba or Gym
+        </button>
+      </div>
+      <hr />
+
+      <div className="fav-playlist">
+        <ul className="playlist">
+          {genre.map(function (playlist) {
+            return (
+              <a href={playlist.link}>
+                <li
+                  key={playlist.name}
+                  styel={{
+                    backgroundPosition: "top 30% center",
+                    backgroundRepeat: "no-repeat",
+                    borderRadius: "1rem"
+                  }}
+                >
+                  <div className="playlist-details">
+                    <h1 id="playlis-name">{playlist.name}</h1>
+                    <div id="artist">{playlist.artist}</div>
+                    <div id="genre">Genre: {playlist.genre}</div>
+                  </div>
+                </li>
+              </a>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
